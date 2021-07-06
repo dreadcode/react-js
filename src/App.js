ae03755b138c2aec6290622ec6b874fb15fe144c
dreadcode <79065684+dreadcode@users.noreply.github.com>
2021-07-06 12:23:33 +0200

    Push a new change
    Test wildcard

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <h2>Make a change</h2>
     </div>
   );
 }
